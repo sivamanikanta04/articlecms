f166999d1080a164cd05ab731f65648aadee0f43
sivamanikanta04 <sivamanikantagudla@gmail.com>
2026-03-06 00:08:01 +0530

    fixed

--- application.py
+++ application.py
@@ -6,10 +6,10 @@ from os import environ
 from FlaskWebProject import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', '0.0.0.0')
+    HOST = environ.get('SERVER_HOST', 'localhost')
     try:
         PORT = int(environ.get('SERVER_PORT', '8000'))
     except ValueError:
         PORT = 8000
 
-    app.run(HOST, PORT)
+    app.run(HOST, PORT,ssl_context='adhoc')
