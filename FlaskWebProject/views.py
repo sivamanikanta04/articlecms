707d39910af47e56d609a1a9bb42f55d9f30f8db
sivamanikanta04 <154608147+sivamanikanta04@users.noreply.github.com>
2026-03-07 13:17:51 +0530

    Add logging for login attempts

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -67,8 +67,10 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.warning(f"FAILED LOGIN ATTEMPT: Username '{form.username.data}'")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info(f"SUCCESSFUL LOGIN: User '{user.username}' logged in")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
